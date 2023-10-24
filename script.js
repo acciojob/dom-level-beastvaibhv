4284dc863bb109d62c79e973cbbe1860edd94315
Vaibhav Bisht <92950515+beastvaibhv@users.noreply.github.com>
2023-10-25 00:31:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const list = document.getElementsByTagName("li");
+for(let i = 0; i < list.length; i++){
+	if(list[i].hasAttribute("id")){
+		alert("The level of the element is:" + (i+1));
+	}
+}
